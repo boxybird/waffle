717002748e732c0d18421bbd6213fab5022bd4a9
Andrew Rhyand <andrew.rhyand@gmail.com>
2022-06-08 08:10:19 -0600

    wip

--- src/bindings/cache.php
+++ src/bindings/cache.php
@@ -3,7 +3,5 @@
 use Illuminate\Cache\CacheManager;
 
 $app->singleton('cache', function () use ($app) {
-    $app['config'] = $app->make('config')->get('cache');
-
     return new CacheManager($app);
 });
--- src/config/cache.php
+++ src/config/cache.php
@@ -1,10 +1,13 @@
 <?php
 
 return [
-    'cache.default'     => 'file',
-    'cache.prefix'      => 'waffle',
-    'cache.stores.file' => [
-        'driver' => 'file',
-        'path'   => WP_CONTENT_DIR . '/waffle-cache',
+    'default' => 'file',
+    'ttl'     => 3600,
+    'prefix'  => 'waffle',
+    'stores'  => [
+        'file' => [
+            'driver' => 'file',
+            'path'   => WP_CONTENT_DIR . '/waffle-cache',
+        ],
     ],
 ];
