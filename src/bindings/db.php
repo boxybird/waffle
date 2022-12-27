1c4f10d41b443fdf08e5830cc5545c2d63f05d41
Andrew Rhyand <andrew.rhyand@gmail.com>
2022-12-27 15:59:09 -0700

    wip

--- src/bindings/db.php
+++ src/bindings/db.php
@@ -6,20 +6,20 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 /**
  * Bind database instance to container
  */
-App::getInstance()->singleton('db', function () {
-    $capsule = new Capsule;
+$capsule = new Capsule;
 
-    $capsule->addConnection([
-        'driver'   => defined('DB_DRIVER') ? DB_DRIVER : 'mysql',
-        'host'     => DB_HOST,
-        'database' => DB_NAME,
-        'username' => DB_USER,
-        'password' => DB_PASSWORD,
-        'charset'  => DB_CHARSET,
-    ]);
+$capsule->addConnection([
+    'driver'   => defined('DB_DRIVER') ? DB_DRIVER : 'mysql',
+    'host'     => DB_HOST,
+    'database' => DB_NAME,
+    'username' => DB_USER,
+    'password' => DB_PASSWORD,
+    'charset'  => DB_CHARSET,
+]);
 
-    $capsule->setAsGlobal();
-    $capsule->bootEloquent();
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
 
+App::getInstance()->singleton('db', function () use ($capsule) {
     return $capsule;
 });
