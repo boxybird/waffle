fedf96fb91c87317336576a83b6496165805b6a2
boxybird <andrew.rhyand@gmail.com>
2025-06-29 08:30:33 -0600

    Add missing return type annotations to Waffle helper functions
    Included type annotations to improve code readability, ensure consistency, and enhance developer experience when using helper functions.

--- src/inc/functions.php
+++ src/inc/functions.php
@@ -1,48 +1,48 @@
 <?php
 
 use BoxyBird\Waffle\App;
-use Illuminate\Support\Str;
+use Illuminate\Http\Client\Factory;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
-use Illuminate\Http\Client\Factory;
+use Illuminate\Support\Str;
 
-/**
- * @see BoxyBird\Waffle
- */
 if (!function_exists('waffle_app')) {
+    /**
+     * @return \BoxyBird\Waffle\App
+     */
     function waffle_app()
     {
         return App::getInstance();
     }
 }
 
-/**
- * @see Illuminate\Http\Request
- */
 if (!function_exists('waffle_request')) {
+    /**
+     * @return \Illuminate\Http\Request
+     */
     function waffle_request()
     {
         return App::getInstance()->make('request');
     }
 }
 
-/**
- * @see Illuminate\Config\Repository
- */
 if (!function_exists('waffle_config')) {
+    /**
+     * @return \Illuminate\Config\Repository
+     */
     function waffle_config()
     {
         return App::getInstance()->make('config');
     }
 }
 
-/**
- * @see Illuminate\Validation\Factory
- */
 if (!function_exists('waffle_validator')) {
+    /**
+     * @return \Illuminate\Validation\Factory
+     */
     function waffle_validator(array $data, array $rules, array $messages = [], array $customAttributes = [])
     {
-        $default_messages = require __DIR__ . '/../config/validation-messages.php';
+        $default_messages = require __DIR__.'/../config/validation-messages.php';
 
         $messages = array_merge($default_messages, $messages);
 
@@ -50,30 +50,31 @@ if (!function_exists('waffle_validator')) {
     }
 }
 
-/**
- * @see Illuminate\Database\Capsule\Manager as Capsule
- */
 if (!function_exists('waffle_db')) {
+    /**
+     * @return \Illuminate\Database\Capsule\Manager
+     */
     function waffle_db()
     {
         return App::getInstance()->make('db');
     }
 }
 
-/**
- * @see Illuminate\Session\SessionManager
- */
+
 if (!function_exists('waffle_session')) {
+    /**
+     * @return \Illuminate\Session\SessionManager
+     */
     function waffle_session()
     {
         return App::getInstance()->make('session');
     }
 }
 
-/**
- * @see Illuminate\Cache\CacheManager
- */
 if (!function_exists('waffle_cache')) {
+    /**
+     * @return \Illuminate\Cache\CacheManager
+     */
     function waffle_cache()
     {
         $cache_manager = App::getInstance()->make('cache');
@@ -84,80 +85,80 @@ if (!function_exists('waffle_cache')) {
     }
 }
 
-/**
- * @see Illuminate\Support\Collection
- */
 if (!function_exists('waffle_collection')) {
+    /**
+     * @return \Illuminate\Support\Collection
+     */
     function waffle_collection(array $items = [])
     {
         return Collection::make($items);
     }
 }
 
-/**
- * @see \Illuminate\Support\Stringable
- */
 if (!function_exists('waffle_str')) {
+    /**
+     * @return \Illuminate\Support\Stringable
+     */
     function waffle_str(string $string)
     {
         return Str::of($string);
     }
 }
 
-/**
- * @see \Illuminate\Support\Arr
- */
 if (!function_exists('waffle_arr')) {
+    /**
+     * @return \Illuminate\Support\Arr
+     */
     function waffle_arr()
     {
         return new Arr();
     }
 }
 
-/**
- * @see Illuminate\Http\Client\Factory
- */
 if (!function_exists('waffle_http')) {
+    /**
+     * @return \Illuminate\Http\Client\Factory
+     */
     function waffle_http()
     {
         return new Factory();
     }
 }
 
-/**
- * @see Illuminate\Encryption\Encrypter;
- */
 if (!function_exists('waffle_encrypter')) {
+    /**
+     * @return \Illuminate\Encryption\Encrypter
+     */
     function waffle_encrypter()
     {
         return App::getInstance()->make('encrypter');
     }
 }
 
-/**
- * @see Illuminate\Filesystem\FilesystemManager;
- */
 if (!function_exists('waffle_storage')) {
+    /**
+     * @return \Illuminate\Filesystem\FilesystemManager
+     */
     function waffle_storage()
     {
         return App::getInstance()->make('storage');
     }
 }
 
-/**
- * @see Illuminate\Queue\DatabaseQueue;
- */
 if (!function_exists('waffle_queue')) {
+    /**
+     * @return \Illuminate\Queue\DatabaseQueue
+     */
     function waffle_queue()
     {
         return App::getInstance()->make('queue');
     }
 }
 
-/**
- * @see BoxyBird\Waffle\Worker;
- */
 if (!function_exists('waffle_worker')) {
+    /**
+     * @return \BoxyBird\Waffle\Worker
+     */
     function waffle_worker(array $queues = ['default'])
     {
         return App::getInstance()->make('queue.worker')->setQueues($queues);
