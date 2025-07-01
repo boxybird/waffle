dbad75009b75c29117faa50fbf400ad24731dea5
boxybird <andrew.rhyand@gmail.com>
2025-07-01 08:25:54 -0600

    Refactor `Request` binding to simplify class references and improve consistency.

--- src/bindings/request.php
+++ src/bindings/request.php
@@ -4,8 +4,8 @@ use BoxyBird\Waffle\App;
 use Illuminate\Http\Request;
 
 /**
- * Create a request from server variables, and bind it to the container; optional
+ * Create a request from server variables and bind it to the container; optional
  */
-App::getInstance()->instance(\Illuminate\Http\Request::class, Request::capture());
+App::getInstance()->instance(Request::class, Request::capture());
 
-App::getInstance()->singleton('request', fn($app) => $app->get(\Illuminate\Http\Request::class));
+App::getInstance()->singleton('request', fn($app) => $app->get(Request::class));
