c2dc17a21ebf0f010d492115ba0be05fec299dde
boxybird <andrew.rhyand@gmail.com>
2025-07-01 08:25:41 -0600

    Refactor event dispatcher binding to use `DispatcherContract` for consistency

--- src/bindings/events.php
+++ src/bindings/events.php
@@ -1,9 +1,10 @@
 <?php
 
 use BoxyBird\Waffle\App;
+use Illuminate\Contracts\Events\Dispatcher as DispatcherContract;
 use Illuminate\Events\Dispatcher;
 use Illuminate\Events\EventServiceProvider;
 
 (new EventServiceProvider(App::getInstance()))->register();
 
-App::getInstance()->instance(\Illuminate\Contracts\Events\Dispatcher::class, new Dispatcher(App::getInstance()));
+App::getInstance()->instance(DispatcherContract::class, new Dispatcher(App::getInstance()));
