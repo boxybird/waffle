e46b51fcbc4035657acd94a3f141659d56b1d0d4
boxybird <andrew.rhyand@gmail.com>
2025-07-01 08:53:44 -0600

    Implement `Application` interface in `App` class with stub methods to align with `Illuminate\Contracts\Foundation\Application`.

--- src/App.php
+++ src/App.php
@@ -3,9 +3,25 @@
 namespace BoxyBird\Waffle;
 
 use Illuminate\Container\Container;
+use Illuminate\Contracts\Foundation\Application;
 
-class App extends Container
+class App extends Container implements Application
 {
+    /**
+     * The base path for the application.
+     */
+    protected string $basePath;
+
+    /**
+     * The current locale.
+     */
+    protected string $locale = 'en';
+
+    /**
+     * Indicates if the application has been bootstrapped before.
+     */
+    protected bool $hasBeenBootstrapped = false;
+
     /**
      * @see \Illuminate\Contracts\Foundation\Application::isDownForMaintenance()
      */
@@ -15,7 +31,7 @@ class App extends Container
     }
 
     /**
-     * @param string|string[] $environments
+     * @param  string|string[]  $environments
      *
      * @see \Illuminate\Contracts\Foundation\Application::environment()
      */
@@ -37,5 +53,173 @@ class App extends Container
     public function getNamespace(): string
     {
         return 'BoxyBird\\Waffle\\';
+    }
+
+    public function version(): string
+    {
+        return '1.0.0';
+    }
+
+    public function basePath($path = ''): string
+    {
+        // STUB ONLY AS WAFFLE PACKAGE DOES NOT CURRENT USE IT
+        return '';
+    }
+
+    public function bootstrapPath($path = ''): string
+    {
+        // STUB ONLY AS WAFFLE PACKAGE DOES NOT CURRENT USE IT
+        return '';
+    }
+
+    public function configPath($path = ''): string
+    {
+        // STUB ONLY AS WAFFLE PACKAGE DOES NOT CURRENT USE IT
+        return '';
+    }
+
+    public function databasePath($path = ''): string
+    {
+        // STUB ONLY AS WAFFLE PACKAGE DOES NOT CURRENT USE IT
+        return '';
+    }
+
+    public function langPath($path = ''): string
+    {
+        // STUB ONLY AS WAFFLE PACKAGE DOES NOT CURRENT USE IT
+        return '';
+    }
+
+    public function publicPath($path = ''): string
+    {
+        // STUB ONLY AS WAFFLE PACKAGE DOES NOT CURRENT USE IT
+        return '';
+    }
+
+    public function resourcePath($path = ''): string
+    {
+        // STUB ONLY AS WAFFLE PACKAGE DOES NOT CURRENT USE IT
+        return '';
+    }
+
+    public function storagePath($path = ''): string
+    {
+        // STUB ONLY AS WAFFLE PACKAGE DOES NOT CURRENT USE IT
+        return '';
+    }
+
+    public function runningInConsole(): bool
+    {
+        return in_array(php_sapi_name(), ['cli', 'phpdbg']);
+    }
+
+    public function runningUnitTests(): bool
+    {
+        return defined('PHPUNIT_RUNNING');
+    }
+
+    public function hasDebugModeEnabled(): bool
+    {
+        return false;
+    }
+
+    public function maintenanceMode(): null
+    {
+        return null;
+    }
+
+    public function registerConfiguredProviders(): void
+    {
+        // STUB ONLY AS WAFFLE PACKAGE DOES NOT CURRENT USE IT
+    }
+
+    public function register($provider, $force = false): void
+    {
+        // STUB ONLY AS WAFFLE PACKAGE DOES NOT CURRENT USE IT
+
+        // if (is_string($provider)) {
+        // $provider = $this->resolveProvider($provider);
+        // }
+
+        // $provider->register();
+
+        // return $provider;
+    }
+
+    public function registerDeferredProvider($provider, $service = null): void
+    {
+        // STUB ONLY AS WAFFLE PACKAGE DOES NOT CURRENT USE IT
+    }
+
+    public function resolveProvider($provider)
+    {
+        return new $provider($this);
+    }
+
+    public function boot(): void
+    {
+        // STUB ONLY AS WAFFLE PACKAGE DOES NOT CURRENT USE IT
+    }
+
+    public function booting($callback): void
+    {
+        // STUB ONLY AS WAFFLE PACKAGE DOES NOT CURRENT USE IT
+    }
+
+    public function booted($callback): void
+    {
+        // STUB ONLY AS WAFFLE PACKAGE DOES NOT CURRENT USE IT
+    }
+
+    public function bootstrapWith(array $bootstrappers): void
+    {
+        // STUB ONLY AS WAFFLE PACKAGE DOES NOT CURRENT USE IT
+
+        // $this->hasBeenBootstrapped = true;
+        // foreach ($bootstrappers as $bootstrapper) {
+        // $this->make($bootstrapper)->bootstrap($this);
+        // }
+    }
+
+    public function getLocale()
+    {
+        return $this->locale;
+    }
+
+    public function getProviders($provider): array
+    {
+        return [];
+    }
+
+    public function hasBeenBootstrapped(): bool
+    {
+        return $this->hasBeenBootstrapped;
+    }
+
+    public function loadDeferredProviders(): void
+    {
+        // STUB ONLY AS WAFFLE PACKAGE DOES NOT CURRENT USE IT
+    }
+
+    public function setLocale($locale): void
+    {
+        // STUB ONLY AS WAFFLE PACKAGE DOES NOT CURRENT USE IT
+
+        // $this->locale = $locale;
+    }
+
+    public function shouldSkipMiddleware(): bool
+    {
+        return false;
+    }
+
+    public function terminating($callback): void
+    {
+        // STUB ONLY AS WAFFLE PACKAGE DOES NOT CURRENT USE IT
+    }
+
+    public function terminate(): void
+    {
+        // STUB ONLY AS WAFFLE PACKAGE DOES NOT CURRENT USE IT
     }
 }
