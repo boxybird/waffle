92b56fe7eeb9d24eb1a012015ad515f403338334
boxybird <andrew.rhyand@gmail.com>
2025-07-06 13:41:22 -0600

    Update `Worker` binding to include `queues` parameter in singleton definition

--- src/bindings/worker.php
+++ src/bindings/worker.php
@@ -3,12 +3,12 @@
 use BoxyBird\Waffle\App;
 use BoxyBird\Waffle\Worker;
 
-App::getInstance()->singleton('queue.worker', function ($app): Worker {
+App::getInstance()->singleton('queue.worker', function ($app, $queues): Worker {
     $queue = $app->get('queue');
     $events = $app->get('events');
     $handler = $app->get('exception.handler');
 
     $isDownForMaintenance = (fn(): false => false);
 
-    return new Worker($queue, $events, $handler, $isDownForMaintenance, $app, null);
+    return new Worker($queue, $events, $handler, $isDownForMaintenance, $app, null, $queues);
 });
