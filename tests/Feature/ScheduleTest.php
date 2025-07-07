0b10eb0c226bc25615a5ca15535177c9e7b9778c
boxybird <andrew.rhyand@gmail.com>
2025-07-06 18:23:39 -0600

    Add tests for executing scheduled tasks and registering custom hook names with `waffle_schedule`

--- tests/Feature/ScheduleTest.php
+++ tests/Feature/ScheduleTest.php
@@ -1,6 +1,6 @@
 <?php
 
-test('it can run a scheduled task', function () {
+test('scheduler can immediately execute scheduled task', function () {
     $value = 1;
 
     waffle_schedule()->call(function () use (&$value) {
@@ -8,4 +8,18 @@ test('it can run a scheduled task', function () {
     })->now();
 
     expect($value)->toBe(2);
+});
+
+test('scheduler can register task with custom hook name', function () {
+    $value = 1;
+
+    waffle_schedule()
+        ->as('namespaced_hook_name')
+        ->call(function () use (&$value) {
+            $value = 2;
+        })
+        ->now();
+
+    expect($value)->toBe(2)
+        ->and(has_action('namespaced_hook_name'))->toBeTrue();
 });
