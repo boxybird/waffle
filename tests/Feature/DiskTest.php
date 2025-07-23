8c08a28984534c4e92b79dc81f86371ea0d128d2
boxybird <andrew.rhyand@gmail.com>
2025-07-23 12:30:39 -0600

    Add feature test for local disk storage functionality

---
+++ tests/Feature/DiskTest.php
@@ -0,0 +1,19 @@
+<?php
+
+test('it creates and verifies a file with local disk driver', function () {
+    $base_root = wp_upload_dir()['basedir'];
+    $base_url = wp_upload_dir()['baseurl'];
+
+    $disk = waffle_storage()->build([
+        'driver' => 'local',
+        'root' => $base_root,
+        'url' => $base_url,
+    ]);
+
+    $disk->put('custom-folder/hello-world.txt', 'Hello World');
+
+    $url = $disk->url('custom-folder/hello-world.txt');
+
+    expect($url)->toBe($base_url.'/custom-folder/hello-world.txt')
+        ->and($disk->exists('custom-folder/hello-world.txt'))->toBeTrue();
+});
