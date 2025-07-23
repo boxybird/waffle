010d19d3fb6675a90cc65eee9fff29680f5520d9
boxybird <andrew.rhyand@gmail.com>
2025-07-23 08:24:18 -0600

    Add feature test for `waffle_process` execution

---
+++ tests/Feature/ProcessTest.php
@@ -0,0 +1,11 @@
+<?php
+
+test('it can run a process and get the result', function () {
+    $result = waffle_process()::run('echo Hello World');
+
+    expect($result->successful())->toBeTrue()
+        ->and($result->failed())->toBeFalse()
+        ->and($result->exitCode())->toBe(0)
+        ->and($result->output())->toContain('Hello World')
+        ->and($result->errorOutput())->toBeEmpty();
+});
